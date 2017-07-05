2cba3c409260419d3e97ec7608e7e84cc29c00fe
Tim Dodge <tim.d@gwbuildings.com>
2017-07-05 16:57:06 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const http = require('http');
+
+const leadOptions = { method: 'GET',
+  url: 'https://api.insight.ly/v2.2/Leads',
+  headers: {
+    'postman-token': '9f3e85f7-41df-9912-a454-998db210e4f9',
+    'cache-control': 'no-cache',
+    authorization: 'Basic ZWMxODc0YmItMzZmMi00ZWRmLWE2MWItYzc4NjU1MzAzN2M1'
+  }
+ };
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function() {
+  console.log('listening on port 3000')
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.get('/contact', function(req, res, next) {
+request(leadOptions, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  const contact = {body}
+})
+.pipe(res);
+});
